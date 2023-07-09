67df2e125840c04adaa8d6848487ad992955457f
Spencer Mortensen <spencer@spencermortensen.com>
2023-07-09 16:52:14 -0400

    0.0.4

--- src/Values.php
+++ src/Values.php
@@ -25,28 +25,43 @@
 
 namespace SpencerMortensen\Theme;
 
+use Exception;
+
 class Values
 {
 	private static $reVariable = "\x03\\{\\$([a-zA-Z0-9_\\-]+)\\}\x03DusSX";
 
 	private $variables;
+	private $evaluated;
 
 	public function __construct ()
 	{
 		$this->variables = [];
-	}
-
-	public function get (string $key): ?string
-	{
-		return $this->variables[$key] ?? null;
+		$this->evaluated = [];
 	}
 
 	public function set (string $key, string $value): void
 	{
-		$this->variables[$key] = $this->expand($value);
+		$this->variables[$key] = $value;
+		$this->evaluated[$key] = false;
 	}
 
-	private function expand (string $input): string
+	public function get (string $key): string
+	{
+		if (!array_key_exists($key, $this->variables)) {
+			$keyName = var_export($key, true);
+			throw new Exception("Missing variable: $keyName");
+		}
+
+		if (!$this->evaluated[$key]) {
+			$this->variables[$key] = $this->evaluate($this->variables[$key]);
+			$this->evaluated[$key] = true;
+		}
+
+		return $this->variables[$key];
+	}
+
+	private function evaluate (string $input): string
 	{
 		$output = '';
 		$n = strlen($input);
@@ -55,9 +70,8 @@ class Values
 		while (preg_match(self::$reVariable, $input, $match, PREG_OFFSET_CAPTURE, $i) === 1) {
 			$iBegin = $match[0][1];
 			$key = $match[1][0];
-			$value = $this->variables[$key] ?? '';
 
-			$output .= substr($input, $i, $iBegin - $i) . $value;
+			$output .= substr($input, $i, $iBegin - $i) . $this->get($key);
 			$i = $iBegin + strlen($match[0][0]);
 		}
 
